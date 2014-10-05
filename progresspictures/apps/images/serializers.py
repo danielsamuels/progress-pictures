6dd8dc88406c0ad86f40e943a186e6ab018a7038
Daniel Samuels <daniel.samuels1@gmail.com>
2014-10-05 20:53:20 +0100

    Update serializers and API views with new URLs.

--- progresspictures/apps/images/serializers.py
+++ progresspictures/apps/images/serializers.py
@@ -3,11 +3,27 @@ from .models import Album, Image
 from rest_framework import serializers
 
 
-class AlbumSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Album
+class ImageSerializer(serializers.ModelSerializer):
 
+    url = serializers.Field('get_absolute_url')
 
-class ImageSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Image
+        fields = ['album', 'file', 'title', 'date_added', 'date_taken', 'url']
+        exclude = ['user']
+
+
+class AlbumSerializer(serializers.ModelSerializer):
+
+    url = serializers.Field('get_absolute_url')
+
+    image_set = ImageSerializer(
+        many=True,
+        required=False,
+    )
+
+    class Meta:
+        model = Album
+        fields = ['title', 'date_added', 'public', 'order', 'image_set', 'url']
+        exclude = ['user']
+        depth = 1
--- progresspictures/apps/images/views.py
+++ progresspictures/apps/images/views.py
@@ -7,6 +7,9 @@ from rest_framework import viewsets
 class AlbumViewSet(viewsets.ModelViewSet):
     serializer_class = AlbumSerializer
 
+    def pre_save(self, obj):
+        obj.user = self.request.user
+
     def get_queryset(self):
         return Album.objects.filter(
             user=self.request.user,
